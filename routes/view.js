2dffa296b91ace9a2a78e6856690cbed12624a97
Niall Larson <nialldizzle@gmail.com>
2020-08-01 23:07:36 -0700

    stats

--- routes/view.js
+++ routes/view.js
@@ -13,4 +13,8 @@ router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public", "exercise.html"));
 });
 
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public", "index.html"));
+});
+
 module.exports = router;
